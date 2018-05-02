8243404b6c2438679d2e55f7847eb5f927cb1741
Rohit Sehgal <rohitsehgal1994@gmail.com>
2018-05-02 09:02:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#This script automatically sets your raspberry pi to tor access point.
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -13,6 +14,7 @@ echo -e "This scripts:
   ${YELLOW}*${NC} The name of wifi interface must be ${GREEN}wlan0${NC}."
 
 
+# You should be running the script as root.
 if [ `whoami` != "root" ]; then
     echo -e "This script must be run as ${RED}root${NC}."
     echo -e "Please type in ${GREEN}sudo !!${NC} to run as root."
@@ -31,9 +33,15 @@ if [ $ans == "y" ];then
 fi
 
 echo "Installing various packages..."
+# Package information
+# tor: for setting up the system using Tor
+# isc-dhcp-server: This is required to allow the raspberry pi to give local ip to connecting devices.
+# iptables-persistent: Saving the state of Iptables for restoring during bootup
+# hostapd: sets up the wifi hotspot.
 apt-get install -y tor hostapd isc-dhcp-server iptables-persistent
 
 
+#Configuration change in DHCP server, make it authoritative so as to make it give IP address to connecting devices.
 sed -e '/option domain-name "example.org";/ s/^#*/#/' -i /etc/dhcp/dhcpd.conf
 sed -e '/option domain-name-servers ns1.example.org, ns2.example.org;/ s/^#*/#/' -i /etc/dhcp/dhcpd.conf
 sed '/^#authoritative;/s/^#//' -i /etc/dhcp/dhcpd.conf
@@ -47,16 +55,19 @@ service isc-dhcp-server start
 
 #Assigning the static IP to the wlan interface
 ifconfig wlan0 down
+# Static IP configuration is stored in interfaces.config
 cat interfaces.config > /etc/networks/interfaces
 ifconfig wlan0 up
 
-
+# copying the wifi hotspot config file to hostapd.conf
 cat hostapd.config > /etc/hostapd/hostapd.conf
 sed 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' > /etc/default/hostapd
 
+# Allow the Rpi to behave as NAT forwarder
 echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
+# Configure IP tables to forward all the traffic on wlan0 to tor service running on 9040 port.
 cat tor.config > /etc/tor/torrc
 iptables -F
 iptables -t nat -F
@@ -64,8 +75,11 @@ iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 
 iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
 iptables -t nat -L
+
+# Saving the state of Iptables.
 iptables-save > /etc/iptables/rules.v4
 
+# Make IP tables ready to restore after bootup.
 cat >> /etc/rc.local << EOF
 iptables-restore < /etc/iptables.conf
 EOF
